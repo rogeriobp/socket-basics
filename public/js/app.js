efbb69e667e635805bae26aa38bcac8f28c16335
Rogerio Peyroton <rogeriobp@gmail.com>
2016-09-20 21:39:01 -0300

    Add name support

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,23 @@
+var name = getQueryVariable('name') || 'Anonymous';
+var room = getQueryVariable('room');
 var socket = io();
+
+console.log(name + ' joined ' + room);
 
 socket.on('connect', function(){
   console.log('connected to socket.io server!');
+
 });
 
 socket.on('message', function(message){
   var momentTimestamp = moment.utc(message.timestamp);
+  var $messages = jQuery('.messages');
+
   console.log('New message:');
   console.log(message.text);
 
-  jQuery('.messages').append('<p>'+ momentTimestamp.local().format('HH:mm - ') + message.text + '</p>');
+  $messages.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('HH:mm - ') + '</strong></p>');
+  $messages.append('<p>' + message.text + '</p>');
 });
 
 // Handles submitting of new message
@@ -21,6 +29,7 @@ $form.on('submit', function (event) {
   var $message = $form.find('input[name=message]')
 
   socket.emit('message', {
+    name: name,
     text: $message.val()
   });
 
